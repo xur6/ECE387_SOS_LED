00139f07b5a2128f177db4d0db61b5bd839dfc38
xur6 <45722507+xur6@users.noreply.github.com>
2019-02-24 20:40:04 -0500

    Add files via upload

---
+++ sos.ino
@@ -0,0 +1,30 @@
+int ledPin = 13;
+// LED connected to digital pin 13
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+  
+}
+void flash(int duration)
+{
+digitalWrite(ledPin, HIGH);
+delay(duration);
+digitalWrite(ledPin, LOW);
+delay(duration);
+
+}
+
+void loop()
+{
+flash(200); flash(200); flash(200);
+// S
+delay(300);
+// otherwise the flashes run together*//
+flash(500); flash(500); flash(500);
+// O
+delay(300);
+flash(200); flash(200); flash(200);
+// S
+delay(1000);
+// wait 1 second before we start
+}
